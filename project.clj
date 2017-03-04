ce40925dd86ddc2c3be8b3acd69eab187eb5b086
Sean Walker <swlkr.rbl@gmail.com>
2017-03-03 21:30:43 -0800

    Use environ to store api key

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
-                 [cheshire "5.7.0"]])
+                 [cheshire "5.7.0"]
+                 [environ "1.1.0"]
+                 [org.clojure/tools.namespace "0.2.11"]]
+  :main tiger.core
+  :plugins [[lein-environ "1.1.0"]])
