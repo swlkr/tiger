b7b800a3b27e1791ffd736dd13f37f208121ec1b
Sean Walker <swlkr.rbl@gmail.com>
2017-03-03 21:31:35 -0800

    Add has-keys? function

--- src/tiger/utils.clj
+++ src/tiger/utils.clj
@@ -2,3 +2,11 @@
 
 (defn flip [f]
   (fn [x y] (f y x)))
+
+(defn has-keys? [m keys]
+  (and
+    (not (nil? m))
+    (not (nil? keys))
+    (map? m)
+    (coll? keys)
+    (every? (partial contains? m) keys)))
