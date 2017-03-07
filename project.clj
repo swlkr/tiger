f08acfce9a186ce30a08ffff94e897ce843c10bc
Sean Walker <swlkr.rbl@gmail.com>
2017-03-06 20:39:46 -0800

    Add vcr-clj

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [http-kit "2.2.0"]
                  [cheshire "5.7.0"]
                  [environ "1.1.0"]
+                 [com.gfredericks/vcr-clj "0.4.9"]
                  [org.clojure/tools.namespace "0.2.11"]]
   :main tiger.core
   :plugins [[lein-environ "1.1.0"]])
